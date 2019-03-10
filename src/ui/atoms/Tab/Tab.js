734ae6794473397fc089024603762f3921179c4a
Egor KhromovE <khromovE@gmail.com>
2019-03-10 11:27:15 +0800

    some fixes

--- src/ui/atoms/Tab/Tab.js
+++ src/ui/atoms/Tab/Tab.js
@@ -4,13 +4,14 @@ import { gray, blue, white } from '../../../lib/colors'
 
 export const Tab = styled.button`
   position: relative;
+  display: block;
   bottom: -1px;
   border: 1px solid ${({ isActive }) => (isActive ? gray.light : 'transparent')};
-  border-bottom: 0;
+  border-bottom: none;
   cursor: pointer;
   font-size: 14px;
   font-weight: bold;
-  padding: 10px 20px;
+  padding: 10px 20px 11px;
   color: ${({ isActive }) => (!isActive ? gray.lighter : blue.light)};
   background-color: ${({ isActive }) => (isActive ? white.medium : 'transparent')};
   margin: 0;
